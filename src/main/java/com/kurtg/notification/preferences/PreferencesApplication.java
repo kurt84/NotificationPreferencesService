4b3e9ae4a5f5a9648154c4eda3d1691c90020434
Kurt Gehl <kgehl@apspayroll.com>
2022-07-05 14:07:55 -0500

    Initial commit

---
+++ src/main/java/com/kurtg/notification/preferences/PreferencesApplication.java
@@ -0,0 +1,13 @@
+package com.kurtg.notification.preferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreferencesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurtg/notification/preferences/PreferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurtg.notification.preferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreferencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
